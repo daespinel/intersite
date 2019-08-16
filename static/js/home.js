2fa1b8dea9dd2d33636c901abbc7b408da307556
David Espinel <david.espinel@inria.fr>
2019-08-16 10:58:21 +0200

    Adjusting js,css and html files

--- static/js/home.js
+++ static/js/home.js
@@ -1,271 +1,102 @@
 /*
- * JavaScript file for the application to demonstrate
- * using the API
+ * JavaScript file for the Home page
  */
 
 // Create the namespace instance
 let ns = {};
 
 // Create the model instance
-ns.model = (function() {
+ns.model = (function () {
     'use strict';
-
-    let $event_pump = $('body');
 
     // Return the API
     return {
-        'read': function() {
+        'read': function () {
             let ajax_options = {
                 type: 'GET',
-                url: 'api/intersite-vertical',
+                url: '/api/intersite-vertical',
                 accepts: 'application/json',
                 dataType: 'json'
             };
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_read_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
-        },
-        create: function(name, type, resources) {
-            console.log("joder");
-            console.info("joder");
-            let ajax_options = {
-                type: 'POST',
-                url: 'api/intersite-vertical',
-                accepts: 'application/json',
-                contentType: 'application/json',
-                dataType: 'json',
-                data: JSON.stringify({
-                    'name': name,
-                    'type': type,
-                    'resources': resources
-                })
-                //data: {'name': name,
-                //       'type': type,
-                //       'resources': resources}
-            };
-            console.log("joder");
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_create_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
-        },
-        update: function(id) {
-            let ajax_options = {
-                type: 'PUT',
-                url: 'api/intersite-vertical/' + id,
-                accepts: 'application/json',
-                contentType: 'application/json',
-                dataType: 'json',
-                data: JSON.stringify({
-                    'id': id
-                })
-            };
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_update_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
-        },
-        'delete': function(id) {
-            let ajax_options = {
-                type: 'DELETE',
-                url: 'api/intersite-vertical/' + id,
-                accepts: 'application/json',
-                contentType: 'plain/text'
-            };
-            $.ajax(ajax_options)
-            .done(function(data) {
-                $event_pump.trigger('model_delete_success', [data]);
-            })
-            .fail(function(xhr, textStatus, errorThrown) {
-                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-            })
+            return $.ajax(ajax_options);
         }
     };
 }());
 
+
 // Create the view instance
-ns.view = (function() {
+ns.view = (function () {
     'use strict';
 
-    let $id = $('#id'),
-        $name = $('#name'),
-        $type = $('#type'),
-        $resources = $('#resources'),
-        $interconnections = $('#interconnections');
+    var $table = $(".blog table");
 
-    // return the API
+    // Return the API
     return {
-        reset: function() {
-            $id.val('');
-            $name.val('');
-            $type.val('');
-            $resources.val('');
-            $interconnections.val('').focus();
-        },
-        update_editor: function(id) {
-            $id.val(id).focus();
-        },
-        build_table: function(service) {
-            let rows = ''
+        build_table: function (data) {
+            let source = $('#blog-table-template').html(),
+                template = Handlebars.compile(source),
+                html;
 
-            // clear the table
-            $('.service table > tbody').empty();
+            // Create the HTML from the template and notes
+            html = template({services: data});
 
-            // did we get a service array?
-            if (service) {
-                for (let i=0, l=service.length; i < l; i++) {
-                    rows += `<tr><td class="id">${service[i].id}</td><td class="name">${service[i].name}</td><td class="type">${service[i].type}</td><td class="resources">${service[i].resources}</td><td class="interconnections">${service[i].interconnections}</td></tr>`;
-                }
-                $('table > tbody').append(rows);
-            }
+            // Append the rows to the table tbody
+            $table.append(html);
         },
-        error: function(error_msg) {
+        error: function (error_msg) {
             $('.error')
                 .text(error_msg)
                 .css('visibility', 'visible');
-            setTimeout(function() {
-                $('.error').css('visibility', 'hidden');
-            }, 3000)
+            setTimeout(function () {
+                $('.error').fadeOut();
+            }, 2000)
         }
     };
 }());
 
-// Create the controller
-ns.controller = (function(m, v) {
+
+// Create the controller instance
+ns.controller = (function (m, v) {
     'use strict';
 
     let model = m,
-        view = v,
-        $event_pump = $('body'),
-        $id = $('#id'),
-        $name = $('#name'),
-        $type = $('#type'),
-        $resources = $('#resources'),
-        $interconnections = $('#interconnections');
+        view = v;
 
-    // Get the data from the model after the controller is done initializing
-    setTimeout(function() {
-        model.read();
-    }, 100)
+    // Get the note data from the model after the controller is done initializing
+    setTimeout(function () {
 
-    // Validate input
-    function validate(type,resources) {
-        return type !== "" && resources !== "";
-    }
+        // Attach event handlers to the promise returned by model.read()
+        model.read()
+            .done(function (data) {
+                view.build_table(data);
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                error_handler(xhr, textStatus, errorThrown);
+            });
+    }, 100);
 
-    // Create our event handlers
-    $('#create').click(function(e) {
-        let name = $name.val(),
-            type = $type.val(),
-            resources = $resources.val();
+    // generic error handler
+    function error_handler(xhr, textStatus, errorThrown) {
+        let error_msg = `${textStatus}: ${errorThrown} - ${xhr.responseJSON.detail}`;
 
-        e.preventDefault();
-
-        if (validate(type, resources)) {
-            model.create(name, type, resources)
-        } else {
-            alert('Problem with type or resources');
-        }
-    });
-
-    $('#update').click(function(e) {
-        let id = $id.val();
-
-        e.preventDefault();
-
-        if (validate(type, resources)) {
-            model.update(type, resources)
-        } else {
-            alert('Problem with type or resources input');
-        }
-        e.preventDefault();
-    });
-
-    $('#delete').click(function(e) {
-        let id = $id.val();
-
-        e.preventDefault();
-
-        if (validate('placeholder', lname)) {
-            model.delete(id)
-        } else {
-            alert('Problem with ID');
-        }
-        e.preventDefault();
-    });
-
-    $('#reset').click(function() {
-        view.reset();
-    })
-
-    $('table > tbody').on('dblclick', 'tr', function(e) {
-        let $target = $(e.target),
-            id,
-            name,
-            type,
-            resources,
-            interconnections;
-
-        id = $target
-            .parent()
-            .find('td.id')
-            .text();
-
-        name = $target
-            .parent()
-            .find('td.name')
-            .text();
-
-        type = $target
-            .parent()
-            .find('td.type')
-            .text();
-
-        resources = $target
-            .parent()
-            .find('td.resources')
-            .text();
-
-        interconnections = $target
-            .parent()
-            .find('td.interconnections')
-            .text();
-
-        view.update_editor(id, name, type, resources, interconnections);
-    });
-
-    // Handle the model events
-    $event_pump.on('model_read_success', function(e, data) {
-        view.build_table(data);
-        view.reset();
-    });
-
-    $event_pump.on('model_create_success', function(e, data) {
-        model.read();
-    });
-
-    $event_pump.on('model_update_success', function(e, data) {
-        model.read();
-    });
-
-    $event_pump.on('model_delete_success', function(e, data) {
-        model.read();
-    });
-
-    $event_pump.on('model_error', function(e, xhr, textStatus, errorThrown) {
-        let error_msg = textStatus + ': ' + errorThrown + ' - ' + xhr.responseJSON.detail;
         view.error(error_msg);
         console.log(error_msg);
-        console.log("joder");
-    })
+    }
+
+    // handle application events
+    $('table').on('dblclick', 'tbody td.name', function (e) {
+        let $target = $(e.target).parent(),
+            person_id = $target.data('person_id');
+
+        window.location = `/people/${person_id}`;
+
+    });
+
+    $('table').on('dblclick', 'tbody td.content', function (e) {
+        let $target = $(e.target).parent(),
+            person_id = $target.data('person_id'),
+            note_id = $target.data('note_id');
+
+        window.location = `people/${person_id}/notes/${note_id}`;
+    });
 }(ns.model, ns.view));
---
+++ static/js/services.js
@@ -0,0 +1,247 @@
+/*
+ * JavaScript file for the application to demonstrate
+ * using the API
+ */
+
+// Create the namespace instance
+let ns = {};
+
+// Create the model instance
+ns.model = (function () {
+    'use strict';
+
+    // Return the API
+    return {
+        read: function () {
+            let ajax_options = {
+                type: 'GET',
+                url: 'api/intersite-vertical',
+                accepts: 'application/json',
+                dataType: 'json'
+            };
+            return $.ajax(ajax_options)
+        },
+        create: function (name, type, resources) {
+            console.log("joder");
+            console.info("joder");
+            let ajax_options = {
+                type: 'POST',
+                url: 'api/intersite-vertical',
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({
+                    'name': name,
+                    'type': type,
+                    'resources': resources
+                })
+            };
+            return $.ajax(ajax_options)
+        },
+        update: function (id) {
+            let ajax_options = {
+                type: 'PUT',
+                url: 'api/intersite-vertical/' + id,
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({
+                    'id': id
+                })
+            };
+            return $.ajax(ajax_options)
+        },
+        'delete': function (id) {
+            let ajax_options = {
+                type: 'DELETE',
+                url: 'api/intersite-vertical/' + id,
+                accepts: 'application/json',
+                contentType: 'plain/text'
+            };
+            return $.ajax(ajax_options)
+        }
+    };
+}());
+
+// Create the view instance
+ns.view = (function () {
+    'use strict';
+
+    const NEW_RESOURCE = 0,
+        EXISTING_RESOURCE = 1;
+
+    let $service_id = $('#id'),
+        $global = $('#global'),
+        $name = $('#name'),
+        $type = $('#type'),
+        $resources = $('#resources'),
+        $interconnections = $('#interconnections'),
+        $create = $('#create'),
+        $update = $('#update'),
+        $delete = $('#delete'),
+        $reset = $('#reset');
+
+    // return the API
+    return {
+        NEW_RESOURCE: NEW_RESOURCE,
+        EXISTING_RESOURCE: EXISTING_RESOURCE,
+        reset: function () {
+            $service_id.val('');
+            $global.val('');
+            $name.val('');
+            $type.val('');
+            $resources.val('');
+            $interconnections.val('').focus();
+        },
+        update_editor: function (id) {
+            $id.val(id).focus();
+        },
+        build_table: function (service) {
+            let source = $('#blog-table-template').html(),
+                template = Handlebars.compile(source),
+                html;
+
+            // clear the table
+            $('.blog table > tbody').empty();
+
+            // did we get a service array?
+            if (service) {
+                html = template({ service: service })
+                $('table > tbody').append(html);
+            }
+        },
+        error: function (error_msg) {
+            $('.error')
+                .text(error_msg)
+                .css('visibility', 'visible');
+            setTimeout(function () {
+                $('.error').css('visibility', 'hidden');
+            }, 3000)
+        }
+    };
+}());
+
+// Create the controller
+ns.controller = (function (m, v) {
+    'use strict';
+
+    let model = m,
+        view = v,
+        $url_service_id = $('#url_service_id')
+        $service_id = $('#service_id'),
+        $service_global = $('#global'),
+        $service_name = $('#name'),
+        $service_type = $('#type'),
+        $service_resources = $('#resources'),
+        $service_interconnections = $('#interconnections');
+
+    // Get the data from the model after the controller is done initializing
+    setTimeout(function () {
+        view.reset();
+        model.read()
+            .done(function (data) {
+                view.build_table(data);
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                error_handler(xhr, textStatus, errorThrown);
+            })
+
+        if ($url_service_id.val() !== "") {
+            model.read_one(parseInt($url_service_id.val()))
+                .done(function (data) {
+                    view.update_editor(data);
+                    view.set_button_state(view.EXISTING_RESOURCE);
+                })
+                .fail(function (xhr, textStatus, errorThrown) {
+                    error_handler(xhr, textStatus, errorThrown);
+                });
+        }
+    }, 100)
+
+    // Validate input
+    function validate(type, resources) {
+        return type !== "" && resources !== "";
+    }
+
+    // Create our event handlers
+    $('#create').click(function (e) {
+        let name = $name.val(),
+            type = $type.val(),
+            resources = $resources.val();
+
+        e.preventDefault();
+
+        if (validate(type, resources)) {
+            model.create(name, type, resources)
+        } else {
+            alert('Problem with type or resources');
+        }
+    });
+
+    $('#update').click(function (e) {
+        let id = $id.val();
+
+        e.preventDefault();
+
+        if (validate(type, resources)) {
+            model.update(type, resources)
+        } else {
+            alert('Problem with type or resources input');
+        }
+        e.preventDefault();
+    });
+
+    $('#delete').click(function (e) {
+        let id = $id.val();
+
+        e.preventDefault();
+
+        if (validate('placeholder', lname)) {
+            model.delete(id)
+        } else {
+            alert('Problem with ID');
+        }
+        e.preventDefault();
+    });
+
+    $('#reset').click(function () {
+        view.reset();
+    })
+
+    $('table').on('dblclick', 'tbody tr', function (e) {
+        let $target = $(e.target).parent(),
+            service_id = $target.data('service_id'),
+            global = $target.data('global')
+            name = $target.data('name'),
+            type = $target.data('type'),
+            resources = $target.data('resources.resource_uuid'),
+            interconnections = $target.data('interconnections');
+
+        view.update_editor({ service_id: service_id, global: global, name: name, type: type, resources: resources, interconnections: interconnections });
+    });
+
+    // Handle the model events
+    $event_pump.on('model_read_success', function (e, data) {
+        view.build_table(data);
+        view.reset();
+    });
+
+    $event_pump.on('model_create_success', function (e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_update_success', function (e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_delete_success', function (e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_error', function (e, xhr, textStatus, errorThrown) {
+        let error_msg = textStatus + ': ' + errorThrown + ' - ' + xhr.responseJSON.detail;
+        view.error(error_msg);
+        console.log(error_msg);
+        console.log("joder");
+    })
+}(ns.model, ns.view));
