6bd3e411bb54229183c48a4a52721ef693ae6663
David Espinel <david.espinel@inria.fr>
2019-11-12 13:37:53 +0100

    Retrieving network elements with adaptor rather than clients

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneauth1.adapter import Adapter
 from keystoneclient.v3 import client as keystoneclient
-from neutronclient.v2_0 import client as neutronclient
-from neutronclient.common import exceptions as neutronclient_exc
 
 FIRST_REGION_NAME = "RegionOne"
 #KEYSTONE_ENDPOINT = "http://{{keystone_ip_node}}/identity/v3"
@@ -13,24 +11,6 @@ KEYSTONE_ENDPOINT = "http://192.168.57.6/identity/v3"
 
 def get_session_object(auth_param):
     return session.Session(auth=auth_param)
-
-
-def get_keystone_catalog(keystone_endpoint):
-    auth = get_auth_object(keystone_endpoint)
-    sess = get_session_object(auth)
-    # Auth process
-    auth.get_access(sess)
-    auth_ref = auth.auth_ref
-    return auth_ref.service_catalog.catalog
-
-
-def get_keystone_client(keystone_endpoint, region):
-    sess = get_session_object(get_auth_object(
-        keystone_endpoint, username, password, project))
-    return keystoneclient.Client(
-        session=sess,
-        region_name=region
-    )
 
 
 def get_auth_object(keystone_endpoint):
@@ -42,51 +22,50 @@ def get_auth_object(keystone_endpoint):
         user_domain_id="default",
         project_domain_id="default",
         include_catalog=True,
+        # Allow fetching a new token if the current one is going to expire
         reauthenticate=True
     )
 
-def get_neutron_client(keystone_endpoint, region):
-    # Use keystone session because Neutron is not yet fully integrated with
-    # Keystone v3 API
-    sess = get_session_object(get_auth_object(keystone_endpoint))
-    return neutronclient.Client(
-        session=sess,
-        region_name=region
-    )
 
+auth = get_auth_object(KEYSTONE_ENDPOINT)
+sess = get_session_object(auth)
+
+# Authenticate
+auth.get_access(sess)
+auth_ref = auth.auth_ref
+#print("Auth token: %s" %auth_ref.auth_token)
+
+catalog_endpoints = auth_ref.service_catalog.catalog
+#print("Service catalog: %s" % catalog_endpoints)
 
 regions_list = {}
-catalog_endpoints = get_keystone_catalog(
-    KEYSTONE_ENDPOINT)
 
 for obj in catalog_endpoints:
     if obj['name'] == 'neutron':
         for endpoint in obj['endpoints']:
-            print(endpoint["url"])
             regions_list[endpoint["region"]] = endpoint["url"]
 
-print(regions_list)
+#print(regions_list)
 
-ks_adap = Adapter(
-    auth=auth,
-    session=sess,
-    service_type='identity',
-    interface='admin',
-    region_name=OS['name'])
-    
-for region_name,region_endpoint in regions_list.items():
-    #neutron_client = get_neutron_client(
-    #    KEYSTONE_ENDPOINT, region_name
-    #)
-    
-    try:
-        network_temp_list = (
-            neutron_client.list_networks()
-        )
+cidrs_region_network_information = {'10.0.1.0/24': {}, '10.0.2.0/24': {}, '10.0.3.0/24': {}, '10.0.4.0/24': {
+}, '10.0.5.0/24': {}, '10.0.6.0/24': {}, '10.0.7.0/24': {}, '10.0.8.0/24': {}, '10.0.9.0/24': {}, '20.0.0.0/24':{}}
 
-    except neutronclient_exc.ConnectionFailed:
-        app_log.info("Can't connect to neutron %s" %
-                     service_remote_inter_endpoints[item])
-    except neutronclient_exc.Unauthorized:
-        app_log.info("Connection refused to neutron %s" %
-                     service_remote_inter_endpoints[item])
+# For every region find the networks created with heat
+for region_name, region_endpoint in regions_list.items():
+    net_adap = Adapter(
+        auth=auth,
+        session=sess,
+        service_type='network',
+        interface='public',
+        region_name=region_name)
+
+    per_region_net_list = net_adap.get('/v2.0/networks').json()
+    region_network = per_region_net_list['networks']
+    
+    # For every network find the cidr of the subnetwork it has
+    for index in range(len(region_network)):   
+        net_ID = region_network[index]['id']
+        subnet_ID = region_network[index]['subnets'][0]
+
+        per_net_subnet = net_adap.get('/v2.0/subnets/'+subnet_ID).json()
+        print(per_net_subnet)
